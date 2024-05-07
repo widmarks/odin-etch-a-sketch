148ef777d8226f511a6133a5e377390631ee6819
widmarks <165108380+widmarks@users.noreply.github.com>
2024-05-07 12:16:32 +0200

    Fix grid size slider and add buttons for color and grid toggle

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const gridBtn = document.querySelector(`#grid-size`);
 const body = document.querySelector(`body`);
 const slider = document.querySelector(`#slider`);
 const sliderText = document.querySelector(`.sliderText`);
+const reset = document.querySelector(`#reset`);
+const color = document.querySelector(`#color`);
+const toggle = document.querySelector(`#toggle`);
+
+let rgbColor = function () {
+  return Math.floor(Math.random() * 255);
+};
 
 sliderText.textContent = slider.value;
 function sliderSize(value) {
@@ -12,26 +19,41 @@ function sliderSize(value) {
 slider.addEventListener(`mousemove`, (e) => {
   sliderSize(e.target.value);
 });
-// slider.addEventListener(`change`, (e) => {
-//   createCanvas(e.target.value);
-// });
-
+slider.addEventListener(`change`, (e) => {
+  createCanvas(e.target.value);
+});
+console.log(rgbColor());
 createCanvas(slider.value);
 
 function createCanvas(num) {
-  const button = document.createElement(`button`);
-  button.textContent = `Reset`;
-  body.appendChild(button);
+  let div = container.querySelectorAll(`div`);
+  div.forEach((divs) => divs.remove());
+  let RGB = false;
+
   for (let i = 0; i < num * num; i++) {
     const div = document.createElement(`div`);
     div.style.width = `${600 / num}px`;
     div.style.height = `${600 / num}px`;
     container.appendChild(div);
     div.addEventListener(`mouseover`, () => {
-      div.style.backgroundColor = `black`;
+      RGB === true
+        ? (div.style.backgroundColor = `rgb(${rgbColor()}, ${rgbColor()}, ${rgbColor()})`)
+        : (div.style.backgroundColor = `black`);
     });
-    button.addEventListener(`click`, () => {
+    reset.addEventListener(`click`, () => {
       div.style.backgroundColor = `white`;
     });
+    toggle.addEventListener(`click`, () => {
+      div.classList.toggle(`gridBorder`);
+    });
   }
+  color.addEventListener(`click`, () => {
+    if (RGB === false) {
+      RGB = true;
+      color.style.color = `rgb(${rgbColor()}, ${rgbColor()}, ${rgbColor()})`;
+    } else if (RGB === true) {
+      RGB = false;
+      color.style.color = ``;
+    }
+  });
 }
